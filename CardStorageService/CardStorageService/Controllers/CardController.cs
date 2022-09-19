690e87b07205e61e3480c535ecfc800af9ca9e19
vavsn <41681585+vavsn@users.noreply.github.com>
2022-09-19 22:07:10 +0700

    Lesson 1. HomeWork. Fixed a bug.

--- CardStorageService/CardStorageService/Controllers/CardController.cs
+++ CardStorageService/CardStorageService/Controllers/CardController.cs
@@ -42,6 +42,7 @@ namespace CardStorageService.Controllers
                 {
                     ClientId = request.ClientId,
                     CardNo = request.CardNo,
+                    Name = request.Name,
                     ExpDate = request.ExpDate,
                     CVV2 = request.CVV2
                 });
