c65aa120d029fb150429cfec2ceb43a18183ae34
vavsn <41681585+vavsn@users.noreply.github.com>
2022-10-10 20:08:04 +0700

    Lesson 7. Homework example of a full-text search demonstration

--- CardStorageService/CardStorageService/Models/Requests/CreateCardResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CardStorageService.Models.Requests
-{
-    public class CreateCardResponse : IOperationResult
-    {
-        public string? CardId { get; set; }
-        public int ErrorCode { get; set; }
-        public string? ErrorMessage { get; set; }
-    }
-}
--- CardStorageService/CardStorageService/Services/ICardRepositoryService.cs
+++
@@ -1,9 +0,0 @@
-﻿using CardStorageService.Data;
-
-namespace CardStorageService.Services
-{
-    public interface ICardRepositoryService : IRepository<Card, string>
-    {
-        IList<Card> GetByCardId(string id);
-    }
-}
